257910aea2f93889c21855f3105791e6642f8004
Igor Kashkuta <ikashkuta@gmail.com>
2016-05-16 02:05:59 +0100

    Add view life updates to Render Component.

--- taknada/Render.swift
+++ taknada/Render.swift
@@ -5,6 +5,10 @@ class Render: Component {
 
 	// MARK: - Public API
 
+	struct ViewHasBecomeAliveFact: Fact {
+		let source: String
+	}
+
 	final var inputs: [Input]?
 
 	final var view: UIView?
@@ -64,6 +68,31 @@ class Render: Component {
 		}
 	}
 
+	final func updateView(alive alive: Bool) {
+		let shouldCreate = (self.view == nil) && alive
+		let shouldDestroy = (self.view != nil) && !alive
+
+		let maybeUpdate: (() -> Void)?
+		if shouldCreate {
+			maybeUpdate = {
+				if self.view != nil { return }
+				self.initView()
+				self.dispatcher.report(ViewHasBecomeAliveFact(source: #function))
+			}
+		} else if shouldDestroy {
+			maybeUpdate = {
+				if self.view == nil { return }
+				self.deinitView()
+			}
+		} else {
+			maybeUpdate = nil
+		}
+
+		if let update = maybeUpdate {
+			self.commitUpdate(update)
+		}
+	}
+
 	// MARK: - To Override
 
 	func createView() -> UIView {
@@ -77,8 +106,33 @@ class Render: Component {
 	}
 
 	override func unregisterSelf() {
+		if self.view != nil {
+			self.deinitView()
+		}
 		self.inputs = nil
 		self.parent = nil
 		SystemLocator.renderSystem?.unregister(self)
+	}
+
+	// MARK: - Private
+
+	private func initView() {
+		self.view = self.createView()
+		self.parent!.view!.addSubview(self.view!)
+		self.attachInputs()
+	}
+
+	private func deinitView() {
+		self.detachInputs()
+		self.view!.removeFromSuperview()
+		self.view = nil
+	}
+
+	private func attachInputs() {
+		self.inputs?.forEach{ $0.attach() }
+	}
+
+	private func detachInputs() {
+		self.inputs?.forEach{ $0.detach() }
 	}
 }
