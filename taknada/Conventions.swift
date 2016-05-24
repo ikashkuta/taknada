561090bbbaf2702ee4c7753f81b5bfd0bed160d8
Igor Kashkuta <ikashkuta@gmail.com>
2016-05-24 21:43:07 +0100

    Indent Convention Tags properly.

--- taknada/Conventions.swift
+++ taknada/Conventions.swift
@@ -2,7 +2,13 @@ import Foundation
 
 protocol ComponentName { }
 
-struct EntityConventionTags {
-	static let mainRender = "MainRender"
-	static let mainLayout = "MainLayout"
+struct ConventionTags {
+	static let mainRender
+		= "MainRender"
+
+	static let mainLayout
+		= "MainLayout"
+	
+	static let mainManager
+		= "MainManager"
 }
