cdac7a7ac101c00de7d784baab21fce14e5b9342
Igor Kashkuta <ikashkuta@gmail.com>
2016-11-16 20:23:15 +0000

    Initial template Xcode project.

---
+++ taknada/AppDelegate.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]?) -> Bool {
+		self.window = UIWindow.init(frame: UIScreen.main.bounds)
+		let rootViewController = UIViewController.init()
+		rootViewController.view.backgroundColor = UIColor.orange
+		self.window?.rootViewController = rootViewController
+		self.window?.makeKeyAndVisible()
+		return true
+	}
+}
